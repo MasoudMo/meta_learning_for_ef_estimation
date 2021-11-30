bd91ea217eb79d428070c73b80ada8dc8bf246ed
won-bae <bwh0324@gmail.com>
2021-11-29 19:14:09 -0500

    updated dataloader

--- src/builders/dataloader_builder.py
+++ src/builders/dataloader_builder.py
@@ -1,28 +1,70 @@
 import random
-from math import floor
+from math import floor, ceil
 from torch.utils.data import random_split, DataLoader
 from src.core.datasets import custom_collate_fn
 
 
-def build(tasks, logger):
+def build_train(data_config, tasks, logger):
     # Randomly sample a task
-    task_name = random.choice(list(tasks.keys()))
+    task_name = random.choice(list(tasks['train'].keys()))
     task = tasks[task_name]
-    logger.info('Those chosen task is: ' + task_name)
-
-    # Randomly choose a context set split size (at least 5% and at most 95%)
-    context_split = random.uniform(0.05, 0.95)
+    logger.info('Task {} is chosen'.format(task_name))
 
     # Randomly split the task into context and target sets
+    max_samples = data_config['max_samples']
+    context_ratio_range = data_config.get(['context_ratio_range'], [0.05, 0.95])
+    context_split = random.uniform(*context_ratio_range)
     context_size = floor(context_split * len(task))
     target_size = len(task) - context_size
-    context_dataset, target_dataset = random_split(task, [context_size, target_size])
+
+    if context_size > max_samples or target_size > max_samples:
+        num_splits = ceil(max(context_size, target_size) / max_samples)
+
+        context_split_size = ceil(context_size / num_splits)
+        target_split_size = ceil(target_size / num_splits)
+
+        splits = [context_split_size] * num_splits
+        splits[-1] = splits[-1] - (context_size % num_splits)
+        splits = splits + ([target_split_size] * num_splits)
+        splits[-1] = splits[-1] - (target_size % num_splits)
+    else:
+        num_splits = 1
+        splits = [context_size, target_size]
+
+    datasets = random_split(task, splits)
+    context_datasets = datasets[:num_splits]
+    target_datasets = datasets[num_splits:]
+
     logger.info(
         'Using a context size of {} and a target size of {}.'.format(context_size, target_size))
 
     # Create data loaders
-    context_dataloader = DataLoader(context_dataset, batch_size=context_size, collate_fn=custom_collate_fn)
-    target_dataloader = DataLoader(target_dataset, batch_size=target_size, collate_fn=custom_collate_fn)
+    dataloaders = []
+    for context_dataset, target_dataset in zip(context_datasets, target_datasets):
+        context_size, target_size = len(context_dataset), len(target_dataset)
+        context_dataloader = DataLoader(context_dataset, batch_size=context_size, collate_fn=custom_collate_fn)
+        target_dataloader = DataLoader(target_dataset, batch_size=target_size, collate_fn=custom_collate_fn)
+        dataloaders.append(
+            {'context': context_dataloader, 'target': target_dataloader})
+
+    return dataloaders
+
+def build_test(data_config, tasks, logger):
+    task_name = list(tasks['test'].keys())[0]
+    task = tasks[task_name]
+    logger.info('Test task {} is loaded'.format(task_name))
+
+    # Randomly split the task into context and target sets
+    context_size = floor(0.3 * len(task))
+    target_size = len(task) - context_size
+
+    context_dataset, target_dataset = random_split(task, [context_size, target_size])
+
+    # Create data loaders
+    context_dataloader = DataLoader(
+        context_dataset, batch_size=1, collate_fn=custom_collate_fn)
+    target_dataloader = DataLoader(
+        target_dataset, batch_size=1, collate_fn=custom_collate_fn)
 
     dataloaders = {
         'context': context_dataloader,
--- src/builders/task_builder.py
+++ src/builders/task_builder.py
@@ -1,3 +1,4 @@
+import os
 from copy import deepcopy
 from src.core import datasets
 
@@ -8,20 +9,21 @@ DATASETS = {
 
 def build(data_config, logger):
     root = data_config['root']
+    image_shape = 128 # default image_shape
 
-    tasks = {}
+    tasks = {'train': {}, 'test': {}}
     task_tmp_names = [key for key in data_config.keys() if 'task' in key]
     for task_tmp_name in task_tmp_names:
         task_config = deepcopy(data_config[task_tmp_name])
         data_name = task_config.pop('name')
-        task_config['dataset_path'] = root
+        task_config['dataset_path'] = os.path.join(root, data_name)
 
         task_name = '_'.join([data_name, task_config['task'], task_config['view']])
-        tasks[task_name] = DATASETS[data_name](**task_config)
+        tasks['train'][task_name] = DATASETS[data_name](**task_config)
+        image_shape = task_config['image_shape'] # store it for the test set
 
+    tasks['test'] = DATASETS['camus'](image_shape=image_shape, task='all_ef', view='all_views')
     logger.infov(
         'Tasks are built - {}'.format(tasks))
     return tasks
-
-
 
