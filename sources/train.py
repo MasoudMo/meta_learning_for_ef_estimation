220ffdcf6f3ae87f709ed379a17952eac2918122
MasoudMo <masoudmokht@gmail.com>
2021-12-01 02:21:57 -0800

    Added evaluation criteria

--- sources/train.py
+++ sources/train.py
@@ -10,6 +10,8 @@ from torch.utils.data import random_split, DataLoader
 import random
 from math import floor, ceil
 from data import custom_collate_fn
+from torch.nn import L1Loss
+from sklearn.metrics import r2_score
 
 
 # Initialize logger
@@ -28,7 +30,7 @@ def main():
 
     R_dim = 128
     epochs = 100
-    max_samples = 10
+    max_samples = 20
 
     # Device to use
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -109,6 +111,7 @@ def main():
                                      device=device,
                                      task='all_ef',
                                      view='all_views')
+    validation_loss = L1Loss()
 
     x_encoder = CustomCNN3D(input_dim=128,
                             n_conv_layers=3,
@@ -161,9 +164,9 @@ def main():
             target_split_size = ceil(target_size/num_splits)
 
             splits = [context_split_size] * num_splits
-            splits[-1] = splits[-1] - (context_size % num_splits)
+            splits[-1] = splits[-1] - (num_splits * context_split_size - context_size)
             splits = splits + ([target_split_size] * num_splits)
-            splits[-1] = splits[-1] - (target_size % num_splits)
+            splits[-1] = splits[-1] - (num_splits * target_split_size - target_size)
 
         else:
             num_splits = 1
@@ -178,50 +181,53 @@ def main():
 
         for i, (context_dataset, target_dataset) in enumerate(zip(context_datasets, target_datasets)):
 
-            # Create data loaders
-            context_dataloader = DataLoader(context_dataset, batch_size=1, collate_fn=custom_collate_fn)
-            target_dataloader = DataLoader(target_dataset, batch_size=1, collate_fn=custom_collate_fn)
+            # Ignore cases where either context or target sets are empty
+            if len(context_dataset) != 0 and len(target_dataset) != 0:
 
-            optimizer.zero_grad()
+                # Create data loaders
+                context_dataloader = DataLoader(context_dataset, batch_size=1, collate_fn=custom_collate_fn)
+                target_dataloader = DataLoader(target_dataset, batch_size=1, collate_fn=custom_collate_fn)
 
-            # Gather all context and target data
-            x_context = list()
-            y_context = list()
-            x_target = list()
-            y_target = list()
-            for x, y in context_dataloader:
-                x_context.append(x_encoder(x))
-                y_context.append(y)
-            x_context = torch.stack(x_context).permute(1, 0, 2)
-            y_context = torch.stack(y_context).unsqueeze(0)
+                optimizer.zero_grad()
 
-            for x, y in target_dataloader:
-                x_target.append(x_encoder(x))
-                y_target.append(y)
-            x_target = torch.stack(x_target).permute(1, 0, 2)
-            y_target = torch.stack(y_target).unsqueeze(0)
+                # Gather all context and target data
+                x_context = list()
+                y_context = list()
+                x_target = list()
+                y_target = list()
+                for x, y in context_dataloader:
+                    x_context.append(x_encoder(x))
+                    y_context.append(y)
+                x_context = torch.stack(x_context).permute(1, 0, 2)
+                y_context = torch.stack(y_context).unsqueeze(0)
 
-            # Run the LNP model
-            output = lnp_model(x_context, y_context, x_target, y_target)
+                for x, y in target_dataloader:
+                    x_target.append(x_encoder(x))
+                    y_target.append(y)
+                x_target = torch.stack(x_target).permute(1, 0, 2)
+                y_target = torch.stack(y_target).unsqueeze(0)
 
-            # Compute the NPML objective
-            loss = loss_func(output, y_target)
+                # Run the LNP model
+                output = lnp_model(x_context, y_context, x_target, y_target)
 
-            # Back propagate
-            loss.backward()
+                # Compute the NPML objective
+                loss = loss_func(output, y_target)
 
-            # Do one optimization step
-            optimizer.step()
+                # Back propagate
+                loss.backward()
 
-            logger.info('Split {} NPML: {}'.format(i, loss.detach().item()))
-            torch.cuda.empty_cache()
+                # Do one optimization step
+                optimizer.step()
+
+                logger.info('Split {} NPML: {}'.format(i, loss.detach().item()))
+                torch.cuda.empty_cache()
 
         x_encoder.eval()
         lnp_model.eval()
 
         with torch.no_grad():
 
-            context_size = floor(0.3 * len(task))
+            context_size = floor(0.3 * len(validation_task))
             target_size = len(validation_task) - context_size
 
             context_dataset, target_dataset = random_split(validation_task, [context_size, target_size])
@@ -249,10 +255,21 @@ def main():
             # Run the LNP model
             output = lnp_model(x_context, y_context, x_target, y_target)
 
+            # Evaluate MAE loss and r2 score
+            p_yCc = output[0]
+            output_samples = p_yCc.sample().squeeze()
+            output_mean = output_samples.mean(dim=0)
+            ef_r2_score = r2_score(y_true=y_target.squeeze().detach().cpu().numpy(),
+                                   y_pred=output_mean.detach().cpu().numpy())
+            ef_mae_loss = validation_loss(y_target.squeeze(), output_mean)
+
             # Compute the NPML objective
             loss = loss_func(output, y_target)
 
-            logger.info('Epoch {} Validation NPML: {}'.format(epoch, loss.detach().item()))
+            logger.info('Epoch {} Validation: NPML = {}  MAE = {}  R2 Score = {}'.format(epoch,
+                                                                                         loss.detach().item(),
+                                                                                         ef_mae_loss.detach().item(),
+                                                                                         ef_r2_score))
 
 
 if __name__ == '__main__':
